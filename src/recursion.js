89e6dd0d98e16b4b8ce5ea1ad8b0a05508948929
daryll mamaril <dazzmamaril@gmail.com>
2021-01-12 19:51:25 +0800

    Implemented 6.Integer Range

--- src/recursion.js
+++ src/recursion.js
@@ -93,7 +93,25 @@ var sumBelow = function(n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+
+let result = [];
+if (x === y || x + 1 === y) {
+  return [];
+}
+if (x === y - 1 || x === y + 1) {
+  return result;
+}
+
+if (x < y) {
+  result.push(x + 1);
+  return result.concat(range(x + 1, y));
+} else {
+  result.push(x - 1);
+  return result.concat(range(x - 1, y))
+}
+
 };
+
 
 // 7. Compute the exponent of a number.
 // The exponent of a number says how many times the base number is used as a factor.
